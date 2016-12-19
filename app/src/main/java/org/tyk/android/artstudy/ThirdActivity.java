41290e23702dd7161f774ecb33bf43dd88573b88
唐杨凯 <tangyangkai@diqi.sh>
2016-12-19 17:11:32 +0800

    PorterDuffXfermode制作圆形图片

--- app/src/main/java/org/tyk/android/artstudy/ThirdActivity.java
+++ app/src/main/java/org/tyk/android/artstudy/ThirdActivity.java
@@ -1,20 +1,23 @@
 package org.tyk.android.artstudy;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.drawable.BitmapDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class ThirdActivity extends AppCompatActivity {
 
-    private MyCircleImageView myCircleImageView;
-    private ImageView img;
-    private Button btn;
+    private ImageView img, circleImg;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +27,13 @@ public class ThirdActivity extends AppCompatActivity {
     }
 
     private void init() {
-        myCircleImageView = (MyCircleImageView) findViewById(R.id.my_circle_img);
-        img = (ImageView) findViewById(R.id.second_img);
-        btn = (Button) findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        img = (ImageView) findViewById(R.id.img);
+        circleImg = (ImageView) findViewById(R.id.circle_img);
+        button = (Button) findViewById(R.id.change);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                Bitmap bitmap = ((BitmapDrawable) myCircleImageView.getDrawable()).getBitmap();
-                Log.e("log", "宽" + bitmap.getWidth() + "高" + bitmap.getHeight());
+                Bitmap bitmap = ((BitmapDrawable) img.getDrawable()).getBitmap();
                 Matrix matrix = new Matrix();
                 float size = Math.min(bitmap.getWidth(), bitmap.getHeight());
                 //x缩放比例
@@ -41,10 +41,24 @@ public class ThirdActivity extends AppCompatActivity {
                 //y缩放比例
                 float y = size / bitmap.getHeight();
                 matrix.setScale(x, y);
+                //缩放以后的bitmap
                 Bitmap newBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, false);
-                Log.e("log", "宽:" + newBitmap.getWidth() + "高:" + newBitmap.getHeight());
-                img.setImageBitmap(newBitmap);
+                int width = newBitmap.getWidth();
+                int height = newBitmap.getHeight();
+                //圆形bitmap
+                Bitmap circleBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(circleBitmap);
+                Paint paint = new Paint();
+                paint.setAntiAlias(true);
+                paint.setColor(Color.BLACK);
+                canvas.drawCircle(width / 2, height / 2, width / 2, paint);
+                PorterDuffXfermode porterDuffXfermode = new PorterDuffXfermode(PorterDuff.Mode.SRC_IN);
+                paint.setXfermode(porterDuffXfermode);
+                canvas.drawBitmap(newBitmap, 0, 0, paint);
+                circleImg.setImageBitmap(circleBitmap);
             }
         });
     }
+
+
 }
